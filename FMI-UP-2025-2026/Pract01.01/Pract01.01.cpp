bbf1b7f00aa5f66f8e037ad46b861fc5a1e9ee5c
Stefani Kocheva <stefani291423@gmail.com>
2025-10-15 16:33:52 +0300

    Added task-1 from Pract01

---
+++ FMI-UP-2025-2026/Pract01.01/Pract01.01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "UP kursut e" << std::endl;
+    std::cout << "mnogo gotin!\n";
+    std::cout << "Da, vsichki nie\n";
+    std::cout << "obichame UP kursa!\n";
+}
